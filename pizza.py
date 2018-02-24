0d252bd3c832a35102391d8383f5f8286a525106
easklund <dat14eas@student.lu.se>
2018-02-24 18:07:01 +0100

    main method with kind of code

--- pizza.py
+++ pizza.py
@@ -1,5 +1,9 @@
 from sys import argv
 
+def try_slice():
+    return true
+
+
 if __name__ == "__main__":
   fname = 'example.in'
   if len(argv) == 2:
@@ -9,6 +13,13 @@ if __name__ == "__main__":
     R, C, L, H = map(int, f.readline().split(' '))
     content = f.readlines()
     pizza = [list(x.strip()) for x in content]
+
+    for x in range(0, R):
+        for y in range(0, C):
+            for s in range(0, 1):  #1 ska ändras till hur många gånger man kan kalla på try_slices
+                if(try_slice(x, y, slice_type(H, s)))
+                    put_slice()
+
 
   print(R, C, L, H)
   print(pizza)

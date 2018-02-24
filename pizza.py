81924e50914225db36d263778a3cf2dccd5ef2de
VictorWinberg <dat14vwi@student.lu.se>
2018-02-24 18:01:09 +0100

    add slice_type

--- pizza.py
+++ pizza.py
@@ -1,4 +1,15 @@
 from sys import argv
+from math import *
+
+def slice_type(H, count = 0):
+  x = floor(sqrt(H))
+  y = H // x
+  for i in range(0, count):
+    if x > y:
+      x -= 1
+    else:
+      y -= 1
+  return x, y
 
 if __name__ == "__main__":
   fname = 'example.in'
@@ -12,3 +23,5 @@ if __name__ == "__main__":
 
   print(R, C, L, H)
   print(pizza)
+
+  print(slice_type(H))

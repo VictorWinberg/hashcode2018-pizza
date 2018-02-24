49f63eff05daa579cf394b8837701918013ad354
antongoransson <anton.goransson95@gmail.com>
2018-02-24 17:58:39 +0100

    Add validate function

--- pizza.py
+++ pizza.py
@@ -1,5 +1,12 @@
 from sys import argv
 
+def validate(nbr_slices, slices, pizza):
+    cells = 0
+    for s in slices:
+        cells += (s[2] - s[0] + 1) * (s[3] - s[1] + 1)
+    m = len(pizza[0]) * len(pizza)
+    return cells / m
+
 if __name__ == "__main__":
   fname = 'example.in'
   if len(argv) == 2:
@@ -10,5 +17,9 @@ if __name__ == "__main__":
     content = f.readlines()
     pizza = [list(x.strip()) for x in content]
 
-  print(R, C, L, H)
-  print(pizza)
+
+
+
+  slices = [[0, 0, 2, 1], [0, 2 ,2, 2], [0, 3, 2, 4]]
+  nbr_slices = len(slices)
+  print(validate(nbr_slices, slices, pizza))

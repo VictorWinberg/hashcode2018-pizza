2db62c110abf6ec22733f1f53978b4f8b0fe39d0
antongoransson <anton.goransson95@gmail.com>
2018-02-24 18:37:43 +0100

    Add cnt_ing function

--- pizza.py
+++ pizza.py
@@ -18,8 +18,30 @@ def validate(slices, pizza):
   m = len(pizza[0]) * len(pizza)
   return cells / m
 
-def try_slice():
-    return true
+def cnt_ing(x, y, slice_x, slice_y, pizza, L):
+  nbr_ing = {}
+  nbr_ing['T'] = 0
+  nbr_ing['M'] = 0
+  for i in range(x + slice_x):
+    for j in range(y + slice_y):
+      if(i < len(pizza) and j < len(pizza[0])):
+        nbr_ing[pizza[i][j]] += 1
+      else:
+        return False
+  return nbr_ing['T'] >= L and nbr_ing['M'] >=L
+
+def try_slice(x, y, slice_x, slice_y, pizza, L):
+  return cnt_ing(x, y, slice_x, slice_y, pizza, L)
+
+def solve(pizza, R, C, L, H):
+  for x in range(0, R):
+    for y in range(0, C):
+      for s in range(0, 1):  #1 ska ändras till hur många gånger man kan kalla på try_slices
+        slice_x, slice_y = slice_type(H, s)
+        if(try_slice(x, y, slice_x, slice_y, pizza, L)):
+          # put_slice()
+          x += 0
+          y += 0
 
 
 if __name__ == "__main__":
@@ -32,18 +54,11 @@ if __name__ == "__main__":
     content = f.readlines()
     pizza = [list(x.strip()) for x in content]
 
-    for x in range(0, R):
-        for y in range(0, C):
-            for s in range(0, 1):  #1 ska ändras till hur många gånger man kan kalla på try_slices
-                if(try_slice(x, y, slice_type(H, s)))
-                    put_slice()
-
-
   print(R, C, L, H)
   print(pizza)
 
   print(slice_type(H))
-
+  solve(pizza, R, C, L, H)
   slices = [[0, 0, 2, 1], [0, 2 ,2, 2], [0, 3, 2, 4]]
   nbr_slices = len(slices)
   print(validate(slices, pizza))
